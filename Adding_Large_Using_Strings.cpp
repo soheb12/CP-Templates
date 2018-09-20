7312d10afe08d8eb26242805d37e547083c62ad1
Sky_Scraper <moinsoheb02@gmail.com>
2018-09-21 01:44:47 +0530

    Create Adding_Large_Using_Strings.cpp

---
+++ Adding_Large_Using_Strings.cpp
@@ -0,0 +1,28 @@
+#define ll long long
+#define pb push_back
+
+string operator+(string s1,string s2)
+{
+    ll n1=s1.size(),n2=s2.size(),carry=0,sum=0;
+    if(n1>n2)
+    swap(s1,s2);
+    reverse(s1.begin(),s1.end());
+    reverse(s2.begin(),s2.end());
+    string res;
+    for(ll i=0;i<n1;i++)
+    {
+        sum = (s1[i]-'0')+(s2[i]-'0')+carry;
+        res.pb(sum%10+'0');
+        carry = sum/10;
+    }
+    for(ll i=n1;i<n2;i++)
+    {
+        sum = (s1[i]-'0')+(s2[i]-'0')+carry;
+        res.pb(sum%10+'0');
+        carry = sum/10;
+    }
+    if(carry)
+    res.pb(carry+'0');
+    reverse(res.begin(),res.end());
+    return res;   
+}
