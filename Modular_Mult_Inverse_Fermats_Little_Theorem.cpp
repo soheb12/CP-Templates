6164b58a390383fb9844a60467f45a86261e740c
Sky_Scraper <moinsoheb02@gmail.com>
2018-09-21 00:47:57 +0530

    Create Modular_Mult_Inverse_Fermats_Little_Theorem.cpp

---
+++ Modular_Mult_Inverse_Fermats_Little_Theorem.cpp
@@ -0,0 +1,27 @@
+ll gcd(ll a,ll b)
+{
+    if(b==0)
+    return a;
+    return gcd(b,a%b);
+}
+
+ll power(ll a,ll b,ll m)
+{
+    ll res = 1;
+    while(b>0)
+    {
+        if(b&1)
+        res = (res*a)%m;
+        b = b>>1;
+        a = (a*a)%m;
+    }
+    return res;
+}
+
+ll mod_inv(ll a,ll m)
+{
+    ll g = gcd(a,m);
+    if(g!=1)
+    return -1;
+    return power(a,m-2,m);
+}
