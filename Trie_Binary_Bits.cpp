b4d188e45e42c94aa3fc00f75fc2543f5b2230e7
Sky_Scraper <moinsoheb02@gmail.com>
2018-10-03 23:51:48 +0530

    Create Trie_Binary_Bits.cpp

---
+++ Trie_Binary_Bits.cpp
@@ -0,0 +1,60 @@
+typedef struct TrieNode
+{
+    TrieNode* bit[2];
+    ll cnt;
+};
+
+TrieNode* head;
+
+TrieNode* getNode()
+{
+    TrieNode* node = new TrieNode();
+    node->bit[0] = NULL;
+    node->bit[1] = NULL;
+    node->cnt=0;
+    return node;
+}
+
+void insert(ll x)
+{
+    TrieNode* temp = head;
+    for(int i=32;i>=0;i--)
+    {
+        int curbit = (x>>i)&1;
+        if(temp->bit[curbit] == NULL)
+        temp->bit[curbit] = getNode();
+        temp = temp->bit[curbit];
+        temp->cnt++;
+    }
+}
+
+void remove(ll x)
+{
+    TrieNode* temp = head;
+    for(int i=32;i>=0;i--)
+    {
+        int curbit = (x>>i)&1;
+        temp = temp->bit[curbit];
+        temp->cnt--;
+    }
+}
+
+ll max_xor(ll x)
+{
+    TrieNode* temp = head;
+    ll ans = 0;
+    for(int i=32;i>=0;i--)
+    {
+        int curbit = (x>>i)&1;
+        if(temp->bit[curbit^1] != NULL && temp->bit[curbit^1]->cnt > 0)
+        {
+            ans += (1LL << i);
+            temp = temp->bit[curbit^1];
+        }
+        else
+        temp = temp->bit[curbit];
+    }
+    return ans;
+}
+
+//Solved This problem : http://codeforces.com/contest/706/problem/D
