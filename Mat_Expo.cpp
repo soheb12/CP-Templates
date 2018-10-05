a664932f2dd90ece74aec673f4b21155b92cfe39
Sky_Scraper <moinsoheb02@gmail.com>
2018-10-05 12:48:59 +0530

    Create Mat_Expo.cpp

---
+++ Mat_Expo.cpp
@@ -0,0 +1,45 @@
+struct mat
+{
+    ll a[2][2];
+};
+
+
+mat matmul(mat x,mat y)
+{
+    mat ans;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            ans.a[i][j] = 0;
+            for(int k=0;k<2;k++)
+            {
+                ans.a[i][j] = (ans.a[i][j] + x.a[i][k] * y.a[k][j])%mod;
+            }
+            
+        }
+    }
+    return ans;
+}
+
+mat power(mat z,ll n)
+{
+    mat res;
+    res.a[0][0] = 1;
+    res.a[0][1] = 0;
+    res.a[1][0] = 0;
+    res.a[1][1] = 1;
+
+    while(n>0)
+    {
+        if(n&1)
+        res = matmul(res,z);
+
+        n = n>>1;
+        z = matmul(z,z);
+    }
+    return res;
+
+}
+
+// Solved this problem : https://www.codechef.com/problems/CKISSHUG
