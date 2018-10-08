165eafae1b8072daa96c842730f55f7185ff7188
Sky_Scraper <moinsoheb02@gmail.com>
2018-10-08 22:46:31 +0530

    Update Java_Prime_BigInteger.java

--- Java_Prime_BigInteger.java
+++ Java_Prime_BigInteger.java
@@ -1,31 +1,27 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 import java.math.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 public class Main
 {
     public static Scanner inp = new Scanner(System.in);
-    public static boolean returnPrime(BigInteger number) {
+	public static boolean returnPrime(BigInteger number) 
+	{
     //check via BigInteger.isProbablePrime(certainty)
     if (!number.isProbablePrime(5))
         return false;
-
     //check if even
     BigInteger two = new BigInteger("2");
     if (!two.equals(number) && BigInteger.ZERO.equals(number.mod(two)))
         return false;
-
     //find divisor if any from 3 to 'number'
     for (BigInteger i = new BigInteger("3"); i.multiply(i).compareTo(number) < 1; i = i.add(two)) { //start from 3, 5, etc. the odd number, and look for a divisor if any
         if (BigInteger.ZERO.equals(number.mod(i))) //check if 'i' is divisor of 'number'
-            return false;
+    		return false;
     }
     return true;
-}
+	}
 
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -43,9 +39,6 @@ public class Main
 		else
 		System.out.println("NO");
 	    }
-
-		
-		
 	}
 	
 }
