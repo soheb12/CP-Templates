6512667ebd0a69c867d7cd6b7f83ed8794489dee
Sky_Scraper <moinsoheb02@gmail.com>
2018-10-08 22:46:04 +0530

    Create Java_Prime_BigInteger.java

---
+++ Java_Prime_BigInteger.java
@@ -0,0 +1,51 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+    public static Scanner inp = new Scanner(System.in);
+    public static boolean returnPrime(BigInteger number) {
+    //check via BigInteger.isProbablePrime(certainty)
+    if (!number.isProbablePrime(5))
+        return false;
+
+    //check if even
+    BigInteger two = new BigInteger("2");
+    if (!two.equals(number) && BigInteger.ZERO.equals(number.mod(two)))
+        return false;
+
+    //find divisor if any from 3 to 'number'
+    for (BigInteger i = new BigInteger("3"); i.multiply(i).compareTo(number) < 1; i = i.add(two)) { //start from 3, 5, etc. the odd number, and look for a divisor if any
+        if (BigInteger.ZERO.equals(number.mod(i))) //check if 'i' is divisor of 'number'
+            return false;
+    }
+    return true;
+}
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int t;
+	    t = inp.nextInt();
+	    for(int i=0;i<t;i++)
+	    {
+	    BigInteger a = inp.nextBigInteger();
+		BigInteger b = inp.nextBigInteger();
+		a = a.multiply(a);
+		b = b.multiply(b);
+		a = a.subtract(b);
+		if(returnPrime(a))
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+	    }
+
+		
+		
+	}
+	
+}
