b52a445e4a3a6c2d55bbd6fac5eafaaa15dfb2aa
Sky_Scraper <moinsoheb02@gmail.com>
2018-12-06 01:37:11 +0530

    Create *MyTemplate*.cpp

---
+++ *MyTemplate*.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define f(a,n) for(ll i=a;i<n;i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define all(v) v.begin(),v.end()
+using namespace std;
+const ll mod=(1e+9)+7;
+const ll size = 1e5+9;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    #ifdef JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    clock_t beg = clock();
+    solve();
+    clock_t end = clock();
+    fprintf(stderr, "%.3f sec\n", double(end - beg) / CLOCKS_PER_SEC);
+    return 0;
+}
