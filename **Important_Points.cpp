5f1bd9b53ea649d17f0b8084be880c071844f2c5
Sky_Scraper <moinsoheb02@gmail.com>
2018-12-07 00:52:13 +0530

    Update **Important_Points.cpp

--- **Important_Points.cpp
+++ **Important_Points.cpp
@@ -1,1 +1,5 @@
 
+->n is Fibonacii number if : 5*n*n+4 or 5*n*n-4 is a perfect square
+->Find nth Fibonacci : f(2 * k) = f(k) * f(k) + f(k - 1) * f(k - 1)       //n is even n = 2*k
+                     f(2 * k + 1) = f(k) * f(k + 1) + f(k - 1) * f(k)   //n is odd and n = 2*k + 1
+->                     
