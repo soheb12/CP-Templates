08017a382c7a5c8765326fe384d01876ad9f42b5
Sky_Scraper <moinsoheb02@gmail.com>
2018-12-07 12:46:11 +0530

    Update *MyTemplate*.cpp

--- *MyTemplate*.cpp
+++ *MyTemplate*.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 const ll mod=(1e+9)+7;
 const ll size = 1e5+9;
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll power(ll a,ll b,ll m){ll res = 1;while(b>0){if(b&1)res = (res*a)%m;b = b>>1;a = (a*a)%m;}return res;}
 
 void solve()
 {
