bea137c7c1655556248c546489b6f7b428240546
Sky_Scraper <moinsoheb02@gmail.com>
2018-12-19 20:26:53 +0530

    add mod_inv to template

--- *MyTemplate*.cpp
+++ *MyTemplate*.cpp
@@ -8,9 +8,10 @@
 #define all(v) v.begin(),v.end()
 using namespace std;
 const ll mod=(1e+9)+7;
-const ll size = 1e5+9;
-ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
-ll power(ll a,ll b,ll m){ll res = 1;while(b>0){if(b&1)res = (res*a)%m;b = b>>1;a = (a*a)%m;}return res;}
+const ll size = 2e5+9;
+ll gcd(ll a,ll b) {if(b==0)return a;return gcd(b,a%b);}
+ll power(ll a,ll b,ll m) {ll res = 1;while(b>0){if(b&1)res = (res*a)%m;b = b>>1;a = (a*a)%m;}return res;}
+ll mod_inv(ll a,ll m) {return power(a,m-2,m);}
 
 void solve()
 {
