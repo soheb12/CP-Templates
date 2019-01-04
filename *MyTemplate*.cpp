15e82a194fe61aae4b5c171224674daa4687def2
Sky_Scraper <moinsoheb02@gmail.com>
2019-01-04 11:05:08 +0530

    added copyright

--- *MyTemplate*.cpp
+++ *MyTemplate*.cpp
@@ -11,7 +11,6 @@ const ll mod=(1e+9)+7;
 const ll size = 2e5+9;
 ll gcd(ll a,ll b) {if(b==0)return a;return gcd(b,a%b);}
 ll power(ll a,ll b,ll m) {ll res = 1;while(b>0){if(b&1)res = (res*a)%m;b = b>>1;a = (a*a)%m;}return res;}
-ll mod_inv(ll a,ll m) {return power(a,m-2,m);}
 
 void solve()
 {
@@ -30,6 +29,6 @@ int main()
     clock_t beg = clock();
     solve();
     clock_t end = clock();
-    fprintf(stderr, "%.3f sec\n", double(end - beg) / CLOCKS_PER_SEC);
+    fprintf(stderr, "%.3f sec, Copyright %c 2019 Skyscraper. \n", double(end - beg) / CLOCKS_PER_SEC , 184);
     return 0;
 }
