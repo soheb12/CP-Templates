69000e17270b7ce70a92614fd96d627f9b1be43a
Sky_Scraper <moinsoheb02@gmail.com>
2019-06-12 19:08:37 +0530

    Update Miscellaneous.cpp

--- Miscellaneous.cpp
+++ Miscellaneous.cpp
@@ -12,6 +12,22 @@ const ll size = 2e5+9;
 ll gcd(ll a,ll b) {if(b==0)return a;return gcd(b,a%b);}
 ll power(ll a,ll b,ll m = mod) {ll res = 1;while(b>0){if(b&1)res = (res*a)%m;b = b>>1;a = (a*a)%m;}return res;}
 
+//Custom Hash for unordered map
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
 // function to find first index >= x
 ll lowerIndex(ll s,ll x) //lower_bound(a+s,a+n,x) - (a+s);// first index >= x
 { 
